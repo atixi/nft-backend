3d54896a6f530934cb7ef13ff7d1916733cbfd1c
Ahmad <joya.abn@gmail.com>
2021-06-30 11:42:02 +0430

    a bug fixed

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -14,7 +14,7 @@ module.exports = ({ env }) => {
           port,
           database,
           username: user,
-          passowrd,
+          password,
           ssl: { rejectUnauthorized: false },
           options: {
             ssl: false,
