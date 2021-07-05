a0971dca376799356dc6430f54491da73abba435
hekmatnajafi <najafihekmat@gmail.com>
2021-07-05 11:25:17 +0430

    added new field (icon) to categories collection.

--- api/nfts/controllers/nfts.js
+++ api/nfts/controllers/nfts.js
@@ -1,4 +1,5 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
   async findOne(ctx) {
   const seaport = strapi.config.functions.openSeaApi.seaport();
@@ -9,5 +10,17 @@ module.exports = {
           tokenId: id
         })
         return OpenSeaAsset;
+      },
+
+      
+      async find(ctx) {
+        let entities;
+        // if (ctx.query._q) {
+        //   entities = await strapi.services.nft.search(ctx.query);
+        // } else {
+          entities = await strapi.services.nft.find();
+        // }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.nft }));
       },
 };
