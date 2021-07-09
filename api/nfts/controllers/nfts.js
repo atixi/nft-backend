a2c2bae36baa56b8fe4aa4459675e767cf6703ba
hekmatnajafi <najafihekmat@gmail.com>
2021-07-09 20:31:51 +0430

    load explore data from opensea is done

--- api/nfts/controllers/nfts.js
+++ api/nfts/controllers/nfts.js
@@ -2,35 +2,50 @@
 const { sanitizeEntity } = require('strapi-utils');
 const axios = require("axios")
 const seaport = strapi.config.functions.openSeaApi.seaport();
+const getAssetsByOwner = (owner) => {
+  return seaport.api.getAssets({
+    owner,
+    limit: 40
+  });
+};
+const getAssetsByOwners = async(owners) => {
+  const promises = [];
+  for (let i = 0; i < owners.length; i++) {
+    const data = await getAssetsByOwner(owners[i])
+    promises.push( data);
+  }
+  return Promise.allSettled(promises);
+};
+const mergeAssetsByOwners = (result) => {
+  let assets = [];
+  for (let i = 0; i < result.length; i++) {
+    assets = [...assets, ...result[i].value.assets];
+  }
+  return assets;
+};
+
 module.exports = {
   async findOne(ctx) {
-      const {id, address, type} = ctx.params;
-            // return await axios.get("https://api.opensea.io/api/v1/asset///")
-          // let {categories} = await strapi.services.nfts.findOne({id}/{address});
-      if(type == "single")
-      {
+      const {id, address} = ctx.params;
         const OpenSeaAsset = await seaport.api.getAsset({
           tokenAddress: address,
           tokenId: id
         })
         return OpenSeaAsset
-      }
-      else if(type == "orders")
-      {
-        // return ctx.params;
-        const orders  = await seaport.api.getAsset({
-          tokenAddress: address,
-          tokenId: id,
-                  // side: 1
-                });
-
-                return orders;
-      }
      
       },
 
       
-      // async find(ctx) {
+      async find(ctx) {
+        const owners = [
+          "0xe5609a6984ece86cb5ab48b13c3ba5a55d173da8",
+          "0xff6539f953eb682d442c70ae0a9e186dd9668ca2",
+        ];          
+     
+        const result = await getAssetsByOwners(owners);
+       const data = mergeAssetsByOwners(result);
+        return data;
+    }
       
       //   // if (ctx.query._q) {
       //   //   entities = await strapi.services.nft.search(ctx.query);
