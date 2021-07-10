f7a50ebfbca327536b46b7e587f800bc01b50cd7
hekmatnajafi <najafihekmat@gmail.com>
2021-07-10 20:03:48 +0430

    removed unused codes

--- api/nfts/controllers/nfts.js
+++ api/nfts/controllers/nfts.js
@@ -46,35 +46,4 @@ module.exports = {
        const data = mergeAssetsByOwners(result);
         return data;
     }
-      
-      //   // if (ctx.query._q) {
-      //   //   entities = await strapi.services.nft.search(ctx.query);
-      //   // } else {
-      //     // return "dfs"
-      //     // let data;
-      //     // let newData;
-      //     // let dataToSend;
-      //     // data  = await strapi.services.nfts.find();
-      //     // // return  entities;
-      //     // data.map( async (order) => {
-      //       // return await axios.get("https://api.opensea.io/api/v1/asset/0x495f947276749ce646f68ac8c248420045cb7b5e/9870243454359818837048173191911104292730407279767673233444380235657864282113/")
-      //       return await seaport.api.getOrders({
-      //         asset_contract_address: "0x495f947276749ce646f68ac8c248420045cb7b5e",
-      //         token_id: "9870243454359818837048173191911104292730407279767673233444380235657864282113",
-      //         side: 1
-      //       })         
-      //       // dataToSend = orders;
-      //       // dataToSend = orders.tokenAddress
-      //   //    })
-      //   // return dataToSend;
-      //   // }
-      //   // return entities;
-      //   // const { orders, count } = await seaport.api.getOrders({
-      //   //   asset_contract_address: tokenAddress,
-      //   //   token_id: token_id,
-      //   //   side: OrderSide.Buy
-      //   // })
-      //   // return orders;
-      //   // return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.nft }));
-      // },
 };
