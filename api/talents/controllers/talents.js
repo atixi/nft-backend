77aed8eaa73073cb2432584c4624da2f37eb1c1b
Ahmad <joya.abn@gmail.com>
2021-07-11 13:40:36 +0430

    fetching the assests by owner

--- api/talents/controllers/talents.js
+++ api/talents/controllers/talents.js
@@ -1,5 +1,5 @@
 "use strict";
-
+const seaport = strapi.config.functions.openSeaApi.seaport();
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -9,8 +9,11 @@ module.exports = {
   async findOne(ctx) {
     const { userName } = ctx.params;
     const entity = await strapi.services.talents.findOne({ userName });
-
-    return { ...entity };
+    const { assets } = await seaport.api.getAssets({
+      owner: entity.walletAddress,
+      limit: 50,
+    });
+    return { ...entity, assets };
     // return sanitizeEntity(entity, { model: strapi.models.collections });
   },
 };
