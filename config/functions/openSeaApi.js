72ede7f8b7ecdff7bd135c9a4f9362b54aaf9e9a
hekmatnajafi <najafihekmat@gmail.com>
2021-08-05 10:00:57 +0430

    here changed the main network to rinkeby test network for testing

--- config/functions/openSeaApi.js
+++ config/functions/openSeaApi.js
@@ -14,7 +14,7 @@ module.exports = {
   //     const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/c2dde5d7c0a0465a8e994f711a3a3c31')
   //     const seaport = new OpenSeaPort(provider, {
   //       networkName: Network.Main,
-  //       apiKey: "2e7ef0ac679f4860bbe49a34a98cf5ac",
+  //       apiKey: "2e7ef0ac679f4860bbe49a34a98cf5ac", 
   //     });
   //     return seaport;
   // }
