e3d5cb0ee9d86bb0a89bdf80aeeae2ccb931f7c9
Ahmad Joya <35627961+ahmadjoya@users.noreply.github.com>
2021-09-07 14:32:39 +0430

    Update collections.js

--- api/collections/controllers/collections.js
+++ api/collections/controllers/collections.js
@@ -11,7 +11,6 @@ module.exports = {
     const { offset } = ctx.query;
     try {
       const { assets } = await seaport.api.getAssets({
-        owner: entity.talent.walletAddress,
         collection: slug,
         limit: 10,
         offset: offset,
